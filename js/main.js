71b2e9f41b2135e7279576fc9facf5ac049fddf0
emrekayik <emrekayik1905@gmail.com>
2023-01-23 18:09:08 +0300

    console'a stillendirilmiş title ekledim

---
+++ js/main.js
@@ -0,0 +1,17 @@
+const consoleStyle = `
+    background-color: rebeccapurple;
+    padding: 12px;
+    color: white;
+    font-family: monospace;
+    border: 1px solid hotpink;
+    font-size: 2em;
+`;
+const consoleStyle2 = `
+    padding: 12px;
+    color: rebeccapurple;
+    font-family: monospace;
+    border: 1px solid hotpink;
+    font-size: 2em;
+`
+console.log("%cStatsEK-WEB", consoleStyle);
+console.log("%cStatsEK-WEB", consoleStyle2);
